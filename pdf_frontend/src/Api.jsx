f9ad109c78ace316c92e0c6e0b7a0b54cb7146bc
saleem <ab588538@gmail.com>
2025-11-27 21:25:53 +0530

    update the api url

--- pdf_frontend/src/Api.jsx
+++ pdf_frontend/src/Api.jsx
@@ -2,7 +2,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || "http://localhost:5050/api",
+  baseURL: import.meta.env.VITE_API_URL || "https://pdfweb-frontend.onrender.com/api",
   withCredentials: false,
 });
 
